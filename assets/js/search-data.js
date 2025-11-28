2a617c038725487cdd10fc8b581bb5b7751ef0ca
zhexianli-usc <72774365+zhexianli-usc@users.noreply.github.com>
2025-11-28 05:51:55 +0000

    Deploying to gh-pages from @ zhexianli-usc/zhexianli-usc.github.io@24990038fcddd3c5ca0b7183415563c6f5003cd4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Teaching experience and philosophy in control, optimization, and systems engineering.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
